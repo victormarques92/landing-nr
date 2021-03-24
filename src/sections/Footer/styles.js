d48072ac6517b2e4cafa638ae29e28b32fc97cf8
Victor Marques <victormatheus92@gmail.com>
2021-03-24 11:50:02 -0300

    Politica add

--- src/sections/Footer/styles.js
+++ src/sections/Footer/styles.js
@@ -6,7 +6,13 @@ export const Container = styled.div`
   padding: 20px 0;
   width: 90%;
 
+  p {
+    margin-top: 10px;
+    cursor: pointer;
+  }
+
   p + p {
+    cursor: none;
     font-size: 12px;
     margin-top: 8px;
   }
@@ -15,4 +21,38 @@ export const Container = styled.div`
 export const Copy = styled.div`
   border-top: 1px solid ${Colors.primary};
   text-align: center;
+`;
+
+export const PoliticaModal = styled.div`
+  h1 {
+    font-size: 28px;
+    font-weight: 600;
+    border-bottom: 1px solid ${Colors.light};
+    margin-bottom: 32px;
+    padding-bottom: 8px;
+  }
+
+  h4 {
+    line-height: 34px;
+  }
+
+  p {
+    font-size: 18px;
+    line-height: 28px;
+    margin-bottom: 32px;
+  }
+
+  span {
+    color: ${Colors.greylight};
+    font-size: 14px;
+  }
+
+  li {
+    font-size: 14px;
+    line-height: 30px;
+  }
+
+  ul {
+    margin-bottom: 32px;
+  }
 `;
